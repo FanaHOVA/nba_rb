fa8e3224d3f1a921b1ea59538c5b44e7a0d9f174
FanaHOVA <fana@alessiofanelli.com>
2017-10-14 21:38:45 +0200

    Add simple specs

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -1,4 +1,10 @@
 require 'spec_helper'
 
-describe 'Teams endpoints' do
+describe NbaRb::Team do
+  it 'loads classes properly' do
+    expect(NbaRb::Team::BaseClass).to respond_to(:new)
+    expect(NbaRb::Team::Dashboard).to respond_to(:new)
+    expect(NbaRb::Team::Lineups).to respond_to(:new)
+    expect(NbaRb::Team::InGamesSplits).to respond_to(:new)
+  end
 end
