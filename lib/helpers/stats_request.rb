ebd350315b886026fe7a0b8bf330b45e5a5cbeb1
FanaHOVA <fana@alessiofanelli.com>
2018-03-28 18:56:48 +0200

    Cleanup headers

--- lib/helpers/stats_request.rb
+++ lib/helpers/stats_request.rb
@@ -3,13 +3,13 @@ module StatsRequest
     res = HTTParty.get(NbaRb::BASE_URL + endpoint,
                        query: params,
                        headers: {
-                        'user-agent': ('Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'),  # noqa: E501
-                        'Dnt': ('1'),
-                        'Accept-Encoding': ('gzip, deflate, sdch'),
-                        'Accept-Language': ('en'),
-                        'origin': ('http://stats.nba.com')
-                      }
-    )
+                         'user-agent' => 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+                         'Dnt' => '1',
+                         'Accept-Encoding' => 'gzip, deflate, sdch',
+                         'Accept-Language' => 'en',
+                         'origin' => 'http://stats.nba.com'
+                       }
+                     )
 
     res.parsed_response
   end
