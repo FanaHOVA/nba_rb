ed24a293567f89e1e37fe3b2865f953f0567de52
FanaHOVA <fana@alessiofanelli.com>
2018-03-30 18:49:00 +0200

    Add VCR testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,15 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'nba_rb'
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+end
+
+RSpec.configure do |config|
+  config.around(:each) do |example|
+    VCR.use_cassette(example.metadata[:description], {}, &example)
+  end
+end
