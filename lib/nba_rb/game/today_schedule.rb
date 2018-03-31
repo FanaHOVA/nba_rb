a6d02b243b3e38ced35ca4f9abeb0f01757a6e5e
FanaHOVA <fana@alessiofanelli.com>
2018-03-31 13:23:02 +0200

    Add helper methods for scoreboardv2 endpoint

--- lib/nba_rb/game/today_schedule.rb
+++ lib/nba_rb/game/today_schedule.rb
@@ -29,6 +29,35 @@ module NbaRb
 
         @data = res['resultSets']
       end
+
+      def arena_and_tv_info
+        create_stats_hash(@data[0])
+      end
+
+      def matchup_teams_info
+        # Each team has its own array of info. The second element in the array
+        # is the # of the game they play in. Example:
+        # [date_of_game, 1, '...', 'Charlotte']
+        # [date_of_game, 1, '...', 'Washington']
+        # The first game is Charlotte @ Washington
+        create_stats_hash(@data[1])
+      end
+
+      def series_standings
+        create_stats_hash(@data[2])
+      end
+
+      def last_meeting
+        create_stats_hash(@data[3])
+      end
+
+      def eastern_conference_standings_today
+        create_stats_hash(@data[4])
+      end
+
+      def western_conference_standings_today
+        create_stats_hash(@data[5])
+      end
     end
   end
 end
